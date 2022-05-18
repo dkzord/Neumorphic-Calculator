77818e6fc8c74cb3830a7205f3c005114002eb75
Fernando Cavalcanti(DK) <comododk@gmail.com>
2022-05-17 23:54:55 -0300

    Feat: change theme to dark ou light

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+const display = document.querySelector('#display');
+const buttons = document.querySelector('button');
+
+const themeToggleBtn = document.querySelector('.theme-toggler');
+const calculator = document.querySelector('.calculator');
+const toggleIcon = document.querySelector('.toggler-icon');
+let isDark = true;
+
+
+themeToggleBtn.onclick = () => {
+  calculator.classList.toggle('dark');
+  themeToggleBtn.classList.toggle('active');
+  isDark = !isDark;
+}
+
+
+console.log(toggleIcon);
+
+
